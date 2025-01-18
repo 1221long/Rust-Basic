4321e7f47b404317b4d7af8b9b8ebaf73d6bf219
fred <11772686@qq.com>
2025-01-18 22:03:53 +0800

    done 5 questions

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -6,13 +6,13 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
 
+// for one variable, we can have some ref and only one mut ref at same time.
 fn main() {
     let mut x = 100;
     let y = &mut x;
+    *y += 100;    
     let z = &mut x;
-    *y += 100;
     *z += 1000;
     assert_eq!(x, 1200);
 }
